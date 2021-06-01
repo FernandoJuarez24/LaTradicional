ac04993269318ba478e09e7efcf21c9e534c86ba
FernandoJuarez24 <57969849+FernandoJuarez24@users.noreply.github.com>
2021-06-01 12:51:03 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,23 @@
+const boton = document.querySelector('.btnMenu');//Clase del Boton o Lineas
+const contenedorLista = document.querySelector('.contenedorLinks');//Contenedor de la Lista
+let activador = true;
+
+
+boton.addEventListener('click', () =>{
+    
+    document.querySelector('.btnMenu i').classList.toggle('fa-times');
+
+    if(activador){
+        contenedorLista.style.left = '0';
+        contenedorLista.style.transition = '0.5s';
+
+        activador = false;
+    }else{
+        activador = false;
+
+        contenedorLista.style.left = '-100%';
+        contenedorLista.style.transition = '0.5s';
+
+        activador = true;
+    }
+});
